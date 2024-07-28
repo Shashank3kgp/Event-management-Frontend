1a1d9863b419652d40d637464c0c4315a92453e1
Shashank3kgp <shashanktupakulaiitkgp2021@gmail.com>
2024-07-28 19:26:47 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import React from 'react';
+import Home from '../src/Pages/Home';
+import Login from '../src/Pages/Login';
+import Register from '../src/Pages/Register';
 
 function App() {
   return (
-    
+    <React.Fragment>
+   <Register />
+   <Login />
+   <Home />    
+   </React.Fragment>
   );
 }
 
